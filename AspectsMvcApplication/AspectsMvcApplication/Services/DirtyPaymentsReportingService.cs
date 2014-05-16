1413292379f16adc3ff790a946763f194c297a99
Barbara Fusińska <barbara.fusinska@gmail.com>
2014-05-17 01:13:54 +0200

    Adding Thread aspect

---
+++ AspectsMvcApplication/AspectsMvcApplication/Services/DirtyPaymentsReportingService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AspectsMvcApplication.Services
+{
+    public class DirtyPaymentsReportingService : IPaymentsReportingService
+    {
+        private readonly ILoggingService _loggingService;
+
+        public DirtyPaymentsReportingService(ILoggingService loggingService)
+        {
+            _loggingService = loggingService;
+        }
+
+        public void ReportPayment()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                _loggingService.Log("Reporting payment");
+            });
+        }
+    }
+}
---
+++ AspectsMvcApplication/AspectsMvcApplication/Services/PaymentsReportingService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AspectsMvcApplication.Services
+{
+    public class PaymentsReportingService : IPaymentsReportingService
+    {
+        private readonly ILoggingService _loggingService;
+
+        public PaymentsReportingService(ILoggingService loggingService)
+        {
+            _loggingService = loggingService;
+        }
+
+        public void ReportPayment()
+        {
+            Thread.Sleep(3000);
+            _loggingService.Log("Reporting payment");
+        }
+    }
+}
